45674520168592d5e3471fe9fc03fe703afc6540
Jacky Feng <jackyfeng530@yahoo.com>
2019-12-09 16:58:11 +0000

    Automatically backed up by Learn

--- js/createStore.js
+++ js/createStore.js
@@ -6,7 +6,16 @@ function createStore() {
     render();
   };
   
-  return { dispatch }
+  
+  function getState() {
+    return state;
+  }
+  
+  return { 
+    dispatch,
+    getState
+    
+  }
 }
 
 function reducer(state = { count: 0 }, action) {

5f4808a3def3827ffcdcc8614b26d2dcccd42598
Jacky Feng <jackyfeng530@yahoo.com>
2019-12-09 16:59:22 +0000

    Automatically backed up by Learn

--- js/createStore.js
+++ js/createStore.js
@@ -1,45 +1,40 @@
 function createStore() {
-  let state
-  
-  function dispatch(action){
+  let state;
+ 
+  function dispatch(action) {
     state = reducer(state, action);
     render();
-  };
-  
-  
+  }
+ 
   function getState() {
     return state;
   }
-  
-  return { 
+ 
+  return {
     dispatch,
     getState
-    
-  }
-}
-
+  };
+};
+ 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
       return { count: state.count + 1 };
-
+ 
     default:
       return state;
   }
 };
-
-let store = createStore();
-store.dispatch({ type: '@@INIT'})
-
-
+ 
 function render() {
   let container = document.getElementById('container');
   container.textContent = store.getState().count;
 };
-
-dispatch({ type: '@@INIT' })
+ 
+let store = createStore();
+store.dispatch({ type: '@@INIT' });
 let button = document.getElementById('button');
-
-button.addEventListener('click', function() {
+ 
+button.addEventListener('click', () => {
     store.dispatch({ type: 'INCREASE_COUNT' });
 })
